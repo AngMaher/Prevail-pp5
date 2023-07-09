47e1aa1053672a2ff8ac690dfb0416d83037ecd5
AngMaher <angelamaher@mail.com>
2023-07-09 09:57:15 +0000

    changed debug to false

--- prevail_weight_loss/settings.py
+++ prevail_weight_loss/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'prevail-weight-loss-fa257f0d4d7b.herokuapp.com', 
