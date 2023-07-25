d4a41c4a1dd8312422f8e365d76f1d1f1ef1f468
AngMaher <angelamaher@mail.com>
2023-07-25 08:56:55 +0000

    add template to print list of classes

--- classes/models.py
+++ classes/models.py
@@ -5,7 +5,7 @@ class Classes(models.Model):
     leader_name = models.CharField(max_length=200, unique=True)
     class_address = models.CharField(max_length=200)
     class_day = models.CharField(max_length=200)
-    class_time = models.DateField(null=True, blank=True)
+    class_time = models.TimeField(blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return self.leader_name
--- classes/views.py
+++ classes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Classes
 
 
 def classes(request):
-    """A view to return the index page"""
-    return render(request, 'classes/classes.html')
+    """A view to return the classes"""
+    class_list = Classes.objects.all()
+
+    return render(request, 'classes/classes.html', {'class_list': class_list})
