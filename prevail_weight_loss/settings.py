4ff7236d5b834b353f2879a85c97606627282ecf
AngMaher <angelamaher@mail.com>
2023-08-04 14:42:16 +0000

    add cache control

--- prevail_weight_loss/settings.py
+++ prevail_weight_loss/settings.py
@@ -185,6 +185,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'prevail-weight-loss'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -207,6 +213,8 @@ if 'USE_AWS' in os.environ:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PRECENTAGE = 10
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
