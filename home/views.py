f130610f8c97eeded4127ae6e80e62a1d3ea7c1d
AngMaher <angelamaher@mail.com>
2023-08-09 14:14:27 +0000

    add mailchimp and privacy policy

--- home/views.py
+++ home/views.py
@@ -5,6 +5,12 @@ def index(request):
     """A view to return the index page"""
     return render(request, 'home/index.html')
 
+
 def about(request):
     """A view to return the how it works page"""
     return render(request, 'home/how_it_works.html')
+
+
+def policy(request):
+    """A view to see the privacy policy"""
+    return render(request, 'home/privacy_policy.html')
