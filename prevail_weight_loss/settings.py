2f3b40519d7ea8cebe0570722b5c9f6fa06c0536
AngMaher <angelamaher@mail.com>
2023-08-15 11:39:10 +0000

    test debug false

--- prevail_weight_loss/settings.py
+++ prevail_weight_loss/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
+# DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'prevail-weight-loss-fa257f0d4d7b.herokuapp.com',
